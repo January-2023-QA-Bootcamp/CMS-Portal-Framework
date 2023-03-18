c08a51c057245902296a8535696351596f28d1c1
TofaelKabir <tofaelkabir@gmail.com>
2023-03-18 00:18:05 -0400

    Branch Day03

--- src/main/java/BaseUtil/BaseClass.java
+++ src/main/java/BaseUtil/BaseClass.java
@@ -11,35 +11,38 @@ import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
 public class BaseClass {
-	WebDriver driver;
+	public WebDriver driver;
 	public HomePage homePage; // or we can use protected type, otherwise it is default type
 	
-	// Before start a test what need to be done
 	@BeforeMethod
 	public void setUp() {
-		// First job is to recognize the location of driver
+		/*
+		// System is a class and setProperty is a method
+		// 1st way, to show the location of chrome driver
+		// This is an absolute path
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tofael\\eclipse-workspace\\gov.cms.portal.january\\driver\\chromedriver.exe");
-		// Please see below 2 line added
+		*/
+		
+		// new
+		// 2nd way, to show the location of the chrome driver
+		// This is a dynamic way (relative path)
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
-		// Then we need to instantiate the driver
 		driver = new ChromeDriver(options);
 		// maximize method maximize the window
-		driver.manage().window().maximize();
+		// driver.manage().window().maximize();
 		// deleteAllCookies do delete the cookies
 		driver.manage().deleteAllCookies();
-		// get method get the trageted url
 		driver.get("https://portal.cms.gov/portal/");
-		// Pageloadtimeout is to wait to load the page for curtain amount of time
+		// We can also use fullscreen() instead of maximize()
+		driver.manage().window().fullscreen();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-		// Implicitly wait is to wait for a web element
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		homePage = new HomePage(driver);
 		
 	}
 	
-	
-	// After a test is done, what need to do
 	@AfterMethod
 	public void tearUp() {
 		driver.quit();
--- src/test/java/testPages/HomePageTest.java
+++ src/test/java/testPages/HomePageTest.java
@@ -1,5 +1,6 @@
 package testPages;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import BaseUtil.BaseClass;
@@ -25,9 +26,108 @@ public class HomePageTest extends BaseClass {
 		homePage.clickPassword();
 	}
 	
-	@Test (enabled = true)
+	@Test (enabled = false)
 	public void clickNewUserRegistrationTest() throws InterruptedException {
 		homePage.clickNewUserRegistration();
 	}
+	
+	// If your Internet is slow, you can see the action of the pageLoadTimeout(), 
+	// you can see it is taking 20 seconds to load the page
+	// and then it shows the error
+	
+	// Test for implicit wait and it shows No such element exception
+	@Test (enabled = false)
+	public void clickIncorrectNewUserRegistrationTest() throws InterruptedException {
+		homePage.clickIncorrectNewUserRegistration();
+	}
+	
+	// This test case will fail, when you remove Pagefactory class from code form HomePage to test this
+	// When PageFactory class is absent, you will find NullPointerException
+	@Test (enabled = false)
+	public void nullPointerExceptionTest() throws InterruptedException {
+		homePage.clickNewUserRegistration();
+	}
+	
+	@Test (enabled = false)
+	public void forgotUserIdTest () {
+		homePage.forgotUserIdClick();
+	}
+	
+	@Test (enabled = false)
+	public void inputTextInUserIdFieldTest () {
+		homePage.inputTextInUserIdField();
+	}
+	
+	@Test(enabled = false)
+	public void inputTextInUserIdAndPasswordFieldThenClickLoginButtonTest () throws InterruptedException {
+		homePage.inputTextInUserIdAndPasswordFieldThenClickLoginButton();
+	}
+	
+	@Test (enabled = false)
+	public void clickUnlockTest() {
+		homePage.clickUnlock();
+	}
+	// Important: Use of xpath as locator
+	// Web Element: New User Registration Button
+	// We can use an webelement directly in the test class, that is also common, but different people like different way
+	@Test(enabled = false)
+	public void use_of_xpath_as_locator_in_new_user_registration_button() throws InterruptedException {
+		driver.findElement(By.xpath("//a[@id='cms-newuser-reg' or @class='cms-newuser-reg']")).click();
+		Thread.sleep(4000);
+	}
+	
+	// This is best scripted in ebay (I will show it later)
+	// Important: Use of linkText as locator
+	// Web Element: forgot User Id
+	@Test(enabled = false)
+	public void use_of_linkText_as_locator_in_forgot_user_id_hyperlink() {
+		driver.findElement(By.linkText("User ID")).click();
+	}
+	
+	// Important: Use of partialLinkText as locator
+	// Web Element: forgot Password
+	@Test(enabled = false)
+	public void use_of_partialLinkText_as_locator_in_forgot_password_hyperlink() {
+		driver.findElement(By.partialLinkText("Passwor")).click();
+	}
+	
+	// Compound class names not permitted, so if even you see class value unique but separated by space, avoid this as class name
+	// it will show error and failed
+	@Test(enabled = false)
+	public void why_we_use_cssSelector_as_locator_in_logo () {
+		driver.findElement(By.className("cms-icon cms-sprite-loggedout ms-3")).click();
+	}
+	
+	// Web Element: New User Registration Button
+	// Important: Use of cssSelector as locator
+	// cssSelector is very important for interview
+	// cssSelector by class --> inside the string, first put html tag, then dot, then write value of the class attribute 
+	@Test(enabled = false)
+	public void role1_use_of_cssSelector_as_locator_in_new_user_registration_button () {
+		driver.findElement(By.cssSelector("a.cms-newuser-reg")).click();
+	}
+	
+	// Web Element: logo
+	// Important: Use of cssSelector as locator
+	// if the class (compund class) contain separate words [gh-tb ui-autocomplete-input], they are actually different class, then
+	// we have to close the gap between classes by putting dot/period between them
+	// cssSelector by class --> htmltag.class name value 
+	// remove the space between words in class value, and replace with dot or period
+	@Test(enabled = true)
+	public void role1_use_of_cssSelector_as_locator_in_logo () {
+		driver.findElement(By.cssSelector("em.cms-icon.cms-sprite-loggedout.ms-3")).click();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
