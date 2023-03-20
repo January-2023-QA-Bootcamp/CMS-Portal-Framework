393c18621a8b6027de8db4bce2a47c8e7ba1e855
TofaelKabir <tofaelkabir@gmail.com>
2023-03-19 23:36:34 -0400

    Branch Day04

--- src/test/java/testPages/HomePageTest.java
+++ src/test/java/testPages/HomePageTest.java
@@ -1,6 +1,10 @@
 package testPages;
 
+import java.awt.Button;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import BaseUtil.BaseClass;
@@ -113,14 +117,185 @@ public class HomePageTest extends BaseClass {
 	// we have to close the gap between classes by putting dot/period between them
 	// cssSelector by class --> htmltag.class name value 
 	// remove the space between words in class value, and replace with dot or period
-	@Test(enabled = true)
-	public void role1_use_of_cssSelector_as_locator_in_logo () {
+	@Test(enabled = false)
+	public void role2_use_of_cssSelector_as_locator_in_logo () {
 		driver.findElement(By.cssSelector("em.cms-icon.cms-sprite-loggedout.ms-3")).click();
 	}
 	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// if you wanna create cssSelector by id 
+	// first html tag, then # (hash), then write the value of id attribute inside the string
+	@Test(enabled = false)
+	public void role3_use_of_cssSelector_as_locator_in_login_button() {
+		driver.findElement(By.cssSelector("button#cms-login-submit")).click();
+	}
+
+	// Above 3 is mostly used 90% cases of css selector
+	// below 3 high level, no need to see if you feel they are tough
 	
+	// Use of cssSelector as locator
+	// Web Element: login Button
+	// high level, you can ignore
+	// cssSelector by another attribute
+	// --> htmltag[any attribute name ='value'] , in xpath - you use // and @, which is absent here 
+	// and you can use any other attribute name except id and class
+	@Test(enabled = false)
+	public void role4_use_of_cssSelector_as_locator_in_login_button () {
+		driver.findElement(By.cssSelector("button[name='Submit Login']")).click();
+	}
 	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// Not important, can ignore
+	// cssSelector by another attribute
+	// --> htmltag.value of class[attribute name='value'] , in xpath you use // and @, which is absent here and you can use attribute except id and class
+	@Test(enabled = false)
+	public void role5_use_of_cssSelector_as_locator_in_login_button() {
+		driver.findElement(By.cssSelector("button.cms-green-button-reg[name='Submit Login']")).click();
+	}
 	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// Not important, can ignore
+	// cssSelector by another attribute
+	// --> htmltag#value of ID[attribute name='value'] , in xpath you use // and @, which is absent here and you can use attribute except id and class
+	@Test(enabled = false)
+	public void role6_use_of_cssSelector_as_locator_in_login_button () {
+		driver.findElement(By.cssSelector("button#cms-login-submit[name='Submit Login']")).click();
+	}
+	
+	// Important: Use of tagName as locator
+	// tag name: we have to go to Amazon and look for table
+	
+	// Web Element: logo
+	// isDisplayed() is an alternate of click() method which is boolean type
+	// isDisplayed() is the method used to verify the presence of a web element within the web page.
+	// Use of isDisplayed() available- image, link, button, text field, check box etc.
+   @Test(enabled = false)
+	public void use_of_isDisplayed_in_logo_image() {
+	   boolean elementDisplayed = driver.findElement(By.cssSelector("em.cms-icon.cms-sprite-loggedout.ms-3")).isDisplayed();
+	   System.out.println("Is the LOGO displayed? Ans: " + elementDisplayed);
+   }
+   
+   // Web Element: login Button
+	// isEnabled() is the method used to verify if the web element is enabled or
+	// disabled within the web page. isEnabled() is primarily used with buttons.
+	// Use of isEnabled(), a boolean type method
+	
+	@Test(enabled = false)
+	public void use_of_isEnabled_in_login_button() throws InterruptedException {
+		boolean loginButtonEnabled = driver.findElement(By.id("cms-login-submit")).isEnabled();
+		System.out.println("Is the Login Button Enable? Ans: " + loginButtonEnabled);
+	}
+	
+	// Web Element: New User Registration
+	// no need next time
+	// another example
+	@Test(enabled = false)
+	public void newUserRegistrationTest() {
+		boolean nurButtonEnabled = driver.findElement(By.xpath("//a[text()='New User Registration']")).isEnabled();
+		System.out.println("Is the New User Registration Button Enable? Ans: " + nurButtonEnabled);
+	}
+		
+    // Both is not working
+	// Web Element: check box
+	@Test (enabled = false)
+	public void use_of_isSelected_in_check_box () {
+		boolean checkBoxSelected = driver.findElement(By.xpath("//label[@id='cms-label-tc']")).isSelected();
+		System.out.println("Is the check Box selected? Ans: " + checkBoxSelected);
+	}
+	
+	// Web Element: Radio button in Forgot User ID 
+	@Test (enabled = false)
+	public void use_of_isSelected_in_radio_button () throws InterruptedException {
+		// first click on Forgot User ID
+		homePage.forgotUserIdClick();
+		Thread.sleep(5000);
+		boolean radioButtonSelected = driver.findElement(By.xpath("(//label[@class='check-radio'])[1]")).isSelected();
+		System.out.println("Is the check Radio Button selected? Ans: " + radioButtonSelected);
+	}
+	
+	// how can you find the title of the page?
+	// Basically inside <head> tag, <title> tag is present, you can get it from there or
+	// right click on an empty place of any page -- click "view page source" -- ctrl+f (to find) 
+	// -- Type 'title' -- find the <title> tag
+	@Test(enabled = false)
+	public void use_of_getTitle_method_in_home_page() {
+		System.out.println("The Title of the Page is: " + driver.getTitle());
+	}
+	
+	// We use New User Registration	
+	@Test(enabled = false)
+	public void use_of_getCurrentURL_method () throws InterruptedException {
+		driver.findElement(By.className("cms-newuser-reg")).click();
+		Thread.sleep(5000);
+		System.out.println("The Title of the Page is: " + driver.getTitle());
+		System.out.println("The current URL is: " + driver.getCurrentUrl());
+	}
+	
+	// We use New User Registration web element to see the text of the web element
+	@Test(enabled = false)
+	public void use_of_getText_method () {
+		WebElement nur = driver.findElement(By.className("cms-newuser-reg"));
+		System.out.println("The Text for the Web Element is: " + nur.getText());
+	}
+	
+	// In User id text field, no text is present, let's see what it shows as text? -- Empty
+	@Test(enabled = false)
+	public void use_of_getText_method_in_userid_field () {
+		WebElement userId = driver.findElement(By.name("user-d"));
+		System.out.println("The Text for the Web Element is: " + userId.getText());
+	}
+	
+	// We use User ID field
+	// getAttribute() actually give the value of the Attribute
+	@Test(enabled = false)
+	public void use_of_getAttribute_method () throws InterruptedException {
+		driver.findElement(By.xpath("//input[@id='cms-login-userId']")).click();
+		// TODO Nasir
+		Thread.sleep(4000);
+		String value01 = driver.findElement(By.xpath("//input[@id='cms-login-userId']")).getAttribute("placeholder");
+		String value02 = driver.findElement(By.xpath("//input[@id='cms-login-userId']")).getAttribute("class");
+		String value03 = driver.findElement(By.xpath("//input[@id='cms-login-userId']")).getAttribute("id");
+		String value04 = driver.findElement(By.xpath("//input[@id='cms-login-userId']")).getAttribute("title");
+		System.out.println("The value of the placeholder attribute is: " + value01);
+		System.out.println("The value of the class attribute is: " + value02);
+		System.out.println("The value of the id attribute is: " + value03);
+		System.out.println("The value of the title attribute is: " + value04);
+	}
+	
+	// Using User Id field
+	@Test(enabled = false)
+	public void use_of_sendKeys_method_then_clear() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@id='cms-login-userId']")).sendKeys("Enthrall IT");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//input[@id='cms-login-userId']")).clear();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//input[@id='cms-login-userId']")).sendKeys("enthrall_12");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//input[@id='cms-login-userId']")).clear();
+		Thread.sleep(4000);		
+	}
+	
+	// Click on forgot password field, then send a value inside "Enter user id" field and click by Enter key
+	@Test(enabled = false)
+	public void use_of_sendKeys_method_then_click_by_enter_key_of_laptop () throws InterruptedException {
+		driver.findElement(By.id("cms-forgot-password")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.id("cms-forgotpwd-userId")).sendKeys("enthrall_12", Keys.ENTER);
+		Thread.sleep(4000);
+	}
+	
+	// Click on forgot password field, then send a value inside "Enter user id" field and click by Return key
+	@Test(enabled = true)
+	public void use_of_sendKeys_method_then_click_by_return_key_of_laptop () throws InterruptedException {
+		driver.findElement(By.id("cms-forgot-password")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.id("cms-forgotpwd-userId")).sendKeys("enthrall_12", Keys.RETURN);
+		Thread.sleep(4000);
+	}
+		
 	
 	
 	
